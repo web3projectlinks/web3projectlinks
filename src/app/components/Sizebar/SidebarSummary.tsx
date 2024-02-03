fa5fed2a3134d578e3c3a3e2d3e6c0e942e682ff
Sparsh Shandilya <sparshshandilya123@gmail.com>
2024-02-03 21:54:25 +0530

    Created navbar and sidebar

---
+++ src/app/components/Sizebar/SidebarSummary.tsx
@@ -0,0 +1,40 @@
+const SidebarSummary = () => {
+  return (
+    <div className="py-5">
+      <details className="group">
+        <summary className="flex items-center gap-1 font-medium cursor-pointer list-none ml-2">
+          <span className="transition group-open:rotate-180">
+            <svg
+              fill="none"
+              height="15"
+              shape-rendering="geometricPrecision"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="1.5"
+              viewBox="0 0 24 24"
+              width="15"
+            >
+              <path d="M6 9l6 6 6-6"></path>
+            </svg>
+          </span>
+          <span className="text-[12px] font-semibold">
+            What is a SAAS platform?
+          </span>
+        </summary>
+        <div className="text-gray-100 mt-3 mx-3 group-open:animate-fadeIn flex items-center gap-1 flex-wrap">
+          {[1, 1, 1, 1, 1, 1].map((_, index) => (
+            <span
+              key={index}
+              className="inline-flex items-center rounded-[3px] bg-gray-700 px-2 py-1 text-xs font-medium text-gray-100 ring-1 ring-inset ring-gray-500/10"
+            >
+              Badge
+            </span>
+          ))}
+        </div>
+      </details>
+    </div>
+  );
+};
+
+export default SidebarSummary;
---
+++ src/app/database/data.ts
@@ -0,0 +1,30 @@
+export const sideBarData = [
+    {
+        title: "Tags",
+        tags: ["EVM", "L1"]
+    },
+    {
+        title: "Official Links",
+        tags: ["Website",
+            "Whitepaper",
+            "Documentation",
+            "Community",
+            "Forum"]
+    },
+    {
+        title: "Block Explorers",
+        tags: ["Etherscan", "Blockchair", "Parsec"]
+    },
+    {
+        title: "Bridges",
+        tags: ["Layer Zero", "Axelar", "Hop", "Connext", "Wormhole", "Stargate"]
+    },
+    {
+        title: "Bounties",
+        tags: ["Bug Bounty Program"]
+    },
+    {
+        title: "Grants",
+        tags: ["Ethereum Foundation"]
+    },
+]
