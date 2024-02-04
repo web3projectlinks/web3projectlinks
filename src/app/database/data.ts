7042f75e8440c10d4723a010731610c36b3b0b22
Sparsh Shandilya <sparshshandilya123@gmail.com>
2024-02-04 17:04:13 +0530

    Added json data file

--- src/app/database/data.ts
+++ src/app/database/data.ts
@@ -1,30 +1,34 @@
 export const sideBarData = [
     {
         title: "Tags",
-        tags: ["EVM", "L1"]
+        tags: ["pos",
+            "smart contracts",
+            "ethereum ecosystem",
+            "layer 1",
+            "evm"]
+    },
+    {
+        title: "Coin Details",
+        tags: ["Ethereum",
+            "ETH",
+            "logo/ethereum.png",
+            "∞"]
     },
     {
         title: "Official Links",
-        tags: ["Website",
-            "Whitepaper",
-            "Documentation",
-            "Community",
-            "Forum"]
+        tags: ["https://ethereum.org/en/",
+            "https://ethereum.org/en/whitepaper/",
+            "https://ethereum.org/en/developers/docs/",
+            "https://ethereum-magicians.org/",
+            "https://github.com/ethereum/go-ethereum"]
     },
     {
-        title: "Block Explorers",
-        tags: ["Etherscan", "Blockchair", "Parsec"]
-    },
-    {
-        title: "Bridges",
-        tags: ["Layer Zero", "Axelar", "Hop", "Connext", "Wormhole", "Stargate"]
-    },
-    {
-        title: "Bounties",
-        tags: ["Bug Bounty Program"]
-    },
-    {
-        title: "Grants",
-        tags: ["Ethereum Foundation"]
+        title: "Social Platforms",
+        tags: ["https://discord.com/invite/ethereum-org",
+            "https://twitter.com/ethereum",
+            "https://reddit.com/r/ethereum",
+            "https://ethereum.org/community/online",
+            "https://coinmarketcap.com/community/profile/Ethereum/",
+            "https://twitter.com/hashtag/Ethereum"]
     },
 ]
